1940415cb5360a53c31192c5c1226aa5cfeecde9
James Lucktaylor <jlucktay+github@gmail.com>
2018-09-11 21:32:31 +0100

    Tidied up

--- examples/hacker-news/hn.go
+++ examples/hacker-news/hn.go
@@ -10,50 +10,28 @@ import (
 )
 
 func main() {
-	// request and parse the front page
-	resp, err := http.Get("https://news.ycombinator.com/")
-
-	if err != nil {
-		panic(err)
+	// Request and parse the front page
+	resp, getErr := http.Get("https://news.ycombinator.com/")
+	if getErr != nil {
+		panic(getErr)
 	}
 
-	root, err := html.Parse(resp.Body)
-
-	if err != nil {
-		panic(err)
+	root, parseErr := html.Parse(resp.Body)
+	if parseErr != nil {
+		panic(parseErr)
 	}
 
-	// grab all articles and print them
+	// Grab all articles and print them
 	articles := scrape.FindAll(root, nodeMatcher)
 
 	for i, article := range articles {
-		fmt.Printf("%2d %s (%s)\n", i, scrape.Text(article), scrape.Attr(article, "href"))
+		fmt.Printf("%2d) %s (%s)\n", i, scrape.Text(article), scrape.Attr(article, "href"))
 	}
 }
 
-// <a href="https://www.atlasobscura.com/places/encryption-lava-lamps" class="storylink">The randomness of this wall of lava lamps helps encrypt up to 10% of internet</a>
-
-// article
-// <*golang.org/x/net/html.Node>
-
-// article.Attr
-// <[]golang.org/x/net/html.Attribute> (length: 2, cap: 2)
-
-// article.Attr[0]
-// <golang.org/x/net/html.Attribute>
-// Namespace:""
-// Key:"href"
-// Val:"https://www.atlasobscura.com/places/encryption-lava-lamps"
-
-// article.Attr[1]
-// <golang.org/x/net/html.Attribute>
-// Namespace:""
-// Key:"class"
-// Val:"storylink"
-
-// define a matcher
+// Define a matcher
 func nodeMatcher(n *html.Node) bool {
-	// must check for nil values
+	// Must check for nil values
 	if n.DataAtom == atom.A && n.Parent != nil && n.Parent.Parent != nil {
 		return scrape.Attr(n.Parent.Parent, "class") == "athing"
 	}
